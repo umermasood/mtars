ac7fea0ae178c2cdae4c2e174784bf569a4628eb
umermasood <umermasood.dev@gmail.com>
2022-07-02 15:18:41 +0500

    get /mtarsrating endpoint setup

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,28 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
-import tmdbApi from "../../api/tmdbApi";
-import apiConfig from "../../api/apiConfig";
+import tmdbApi from '../../api/tmdbApi';
+import apiConfig from '../../api/apiConfig';
 
-import "./detail.scss";
-import CastList from "./CastList";
+import './detail.scss';
+import CastList from './CastList';
 // import VideoList from './VideoList';
 
 // import MovieList from '../../components/movie-list/MovieList';
 
 const Detail = () => {
   const { category, id } = useParams();
-
   const [item, setItem] = useState(null);
+  const [mtarsRating, setMtarsRating] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const getDetail = async () => {
       const response = await tmdbApi.detail(category, id, { params: {} });
       setItem(response);
+      setLoading(true);
+      console.log(loading);
       window.scrollTo(0, 0);
     };
     getDetail();
-  }, [category, id]);
+
+    const getMTARSRating = async () => {
+      let query = '/mtarsrating?movie=' + item.title;
+      const response = await fetch(query);
+      const jsonData = await response.json();
+      setMtarsRating(jsonData);
+    };
+
+    if (loading) {
+      getMTARSRating();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [category, id, loading]);
 
   return (
     <>
@@ -58,6 +73,7 @@ const Detail = () => {
                   ))}
               </div>
               <p className='overview'>{item.overview}</p>
+              <p>{mtarsRating ? mtarsRating.response : ''}</p>
               <div className='cast'>
                 <div className='section__header'>
                   <h2>Casts</h2>

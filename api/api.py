b0f3799e415f7096d8fa587b31724c2e82697c8d
umermasood <umermasood.dev@gmail.com>
2022-07-23 12:37:51 +0500

    Added root app route

--- api/api.py
+++ api/api.py
@@ -19,6 +19,7 @@ app = Flask(__name__, static_folder='../build', static_url_path='/')
 # ---------------------------------------------------------------------------- #
 #                              ROOT ENDPOINT
 # ---------------------------------------------------------------------------- #
+@app.route('/')
 def index():
     return app.send_static_file('index.html')
 
